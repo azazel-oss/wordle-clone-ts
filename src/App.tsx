d6ffdd17194ce0d829d13ac98d35ff659192264c
Asad Mahmood <asadmhmd1234@gmail.com>
2022-09-30 01:42:06 +0530

    Add enter and backspace functionality

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   const [currentGuess, setCurrentGuess] = useState(0);
   const [guessList, setGuessList] = useState<string[]>(new Array(6).fill(""));
   const keyPressHandler = (event: KeyboardEvent) => {
+    if (currentGuess > 5) return;
     if (event.key.match(/^[a-zA-Z]$/)) {
       if (guessList[currentGuess].length < 5) {
         setGuessList((prevList) => {
@@ -15,15 +16,24 @@ function App() {
         });
       }
     }
+    if (event.key === "Backspace" && guessList[currentGuess].length > 0) {
+      setGuessList((prevList) => {
+        const updatedList = [...prevList];
+        updatedList[currentGuess] = updatedList[currentGuess].slice(0, -1);
+        return updatedList;
+      });
+    }
+    if (event.key === "Enter" && guessList[currentGuess].length === 5) {
+      setCurrentGuess((prevGuess) => prevGuess + 1);
+    }
   };
   useEffect(() => {
-    console.log("bad things happening");
     document.body.addEventListener("keydown", keyPressHandler);
 
     return () => {
       document.body.removeEventListener("keydown", keyPressHandler);
     };
-  }, [guessList]);
+  }, [guessList, currentGuess]);
   return (
     <main className="App">
       <h1>Wordle</h1>
