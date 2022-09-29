39f0a9eb5d68a863a05d92f23a2a810ff7f473a7
Asad Mahmood <asadmhmd1234@gmail.com>
2022-09-30 01:31:36 +0530

    Add key press event to the app and fix cell bug

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,33 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [currentGuess, setCurrentGuess] = useState("");
-  const [guessList, setGuessList] = useState(new Array(6).fill(""));
-  const keyPressHandler = (event: KeyboardEvent) => {};
+  const [currentGuess, setCurrentGuess] = useState(0);
+  const [guessList, setGuessList] = useState<string[]>(new Array(6).fill(""));
+  const keyPressHandler = (event: KeyboardEvent) => {
+    if (event.key.match(/^[a-zA-Z]$/)) {
+      if (guessList[currentGuess].length < 5) {
+        setGuessList((prevList) => {
+          const updatedList = [...prevList];
+          updatedList[currentGuess] += event.key.toLowerCase();
+          return updatedList;
+        });
+      }
+    }
+  };
   useEffect(() => {
+    console.log("bad things happening");
     document.body.addEventListener("keydown", keyPressHandler);
 
     return () => {
       document.body.removeEventListener("keydown", keyPressHandler);
     };
-  }, []);
+  }, [guessList]);
   return (
     <main className="App">
       <h1>Wordle</h1>
 
       {guessList.map((guess, idx) => (
-        <WordRow key={idx} />
+        <WordRow key={idx} word={guess} />
       ))}
     </main>
   );
--- src/components/WordRow.tsx
+++ src/components/WordRow.tsx
@@ -2,23 +2,21 @@ import Cell from "./Cell";
 import styles from "./WordRow.module.css";
 
 type Props = {
-  word?: string;
+  word: string;
 };
 
 const WordRow: React.FC<Props> = (props) => {
+  const auxWordArray = new Array(5).fill("");
+  for (let i = 0; i < auxWordArray.length; i++) {
+    if (props.word[i]) {
+      auxWordArray[i] = props.word[i];
+    }
+  }
   return (
     <div className={styles.row}>
-      {props.word ? (
-        props.word.split("").map((char, idx) => <Cell key={idx} value={char} />)
-      ) : (
-        <>
-          <Cell />
-          <Cell />
-          <Cell />
-          <Cell />
-          <Cell />
-        </>
-      )}
+      {auxWordArray.map((char, idx) => (
+        <Cell key={idx} value={char} />
+      ))}
     </div>
   );
 };
